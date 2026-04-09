aa8b2d64a20d8b19055dc0dc31284756dc0f7758
Dom <dommeld@gmail.com>
2026-04-09 06:42:46 +0000

    ok

--- todo.js
+++ todo.js
@@ -1,36 +1,21 @@
-const apiURL = 'todo_api.php';
+const apiUrl = 'todo_api.php';
 
 document.getElementById('todo-form').addEventListener('submit', function (e) {
- 
     e.preventDefault();
  
     const todoInput = document.getElementById('todo-input').value;
- 
     fetch(apiUrl, {
-
         method: 'POST',
-
         headers: {
-
             'Content-Type': 'application/json',
-
         },
-
         body: JSON.stringify({ todo: todoInput }),
-
     })
-
     .then(response => response.json())
-
     .then((data) => {
-
         console.log(data);
-
         fetchTodos();
-
         document.getElementById('todo-input').value = '';
-
     });
-
 });
  
